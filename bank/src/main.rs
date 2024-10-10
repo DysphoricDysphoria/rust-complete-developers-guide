7aa58d3c53a7396bf843b209647ae356af635c2f
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-10 16:45:22 +0530

    Add 2 more rules for borrowing concept; add code to understand it.

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -31,33 +31,41 @@ impl Bank {
     }
 }
 
+// This func accepts a reference
 fn print_account(account: &Account) {
     println!("{:#?}", account);
 }
 
+// This func accepts a reference
 fn print_account_list_with_ref(accounts: &Vec<Account>) {
     println!("{:#?}", accounts);
 }
 
+// This func accepts a value
 fn print_account_list_without_ref(accounts: Vec<Account>) {
     println!("{:#?}", accounts);
 }
 
-fn ref_demo_0() {
-    let account = Account::new(1, String::from("John Wick"));
+// This func accepts a mutable reference
+fn change_account(account: &mut Account) {
+    account.balance = 10;
+}
 
-    // Multiple references can co-exist for a value
+fn ref_demo_0() {
+    let account = Account::new(1, String::from("John Wick 1"));
+
+    // Multiple immutable references can co-exist for a value
     let account_ref_1 = &account;
     let account_ref_2 = &account;
 
-    // account_ref_1.balance = 100; // !Error: Can't use reference to update value
+    // account_ref_1.balance = 100; // !Error: Can't use immutable reference to update value
 
     // let other_account = account; // !Error: Can't move a value while reference(s) to the value exist (Caveat: Can move if the reference isn't used later on)
 
     print_account(account_ref_1);
     print_account(account_ref_2);
 
-    // println!("{:#?}", account);
+    println!("{:#?}", account);
 }
 
 fn ref_demo_1() {
@@ -83,10 +91,29 @@ fn ref_demo_1() {
     // println!("{:#?}", bank); // !Error: borrow of partially move value: `bank`
 }
 
+fn ref_demo_2() {
+    let mut account = Account::new(1, String::from("John Wick 3"));
+
+    // let account_ref_read_only = &account; // !Error: We can't have a read-only (immutable) reference if we have a mutable reference
+
+    // Mutable reference
+    let account_ref_mutable_1 = &mut account;
+
+    // account.balance = 100; // !Error: Can't modify a binding via its owner if we have a reference (mutable or immutable)
+
+    // let account_ref_mutable_2 = &mut account; // !Error: We can't have another mutable reference if we already have one
+
+    change_account(account_ref_mutable_1);
+
+    // println!("{:#?}", account_ref_read_only.holder); // Here we are using a immutable reference although we already have a mutable reference
+}
+
 fn main() {
     ref_demo_0();
 
     ref_demo_1();
+
+    ref_demo_2();
 }
 
 /*
@@ -97,11 +124,18 @@ fn main() {
 */
 
 /*
+    ### ### ### ### ### ### #
     ### Rules - Ownership ###
-    1. Every value is 'owned' by a single variable (binding), struct, vector, etc. at a time
-    2. Reassigning the value to a variable (binding), passing it to a function, putting it into a vector, etc. moves the value. The old owner can't be used to access the value anymore!
+    ### ### ### ### ### ### #
+        1. Every value is 'owned' by a single variable (binding), struct, vector, etc. at a time
+        2. Reassigning the value to a variable (binding), passing it to a function, putting it into a vector, etc. moves the value. The old owner can't be used to access the value anymore!
 
+    ### ### ### ### ### ### #
     ### Rules - Borrowing ###
-    3. You can create many read-only (immutable) references to a value. These refs can all exist at the same time.
-    4. You can't move a value while a ref to the value exists. (Caveat: Can move if the reference isn't used later on)
+    ### ### ### ### ### ### #
+        3. You can create many read-only (immutable) references to a value. These refs can all exist at the same time.
+        4. You can't move a value while a ref to the value exists. (Caveat: Can move if the reference isn't used later on)
+
+        5. You can make a writeable (mutable) reference to a value only if there are no read-only references currently in use. One mutable ref to a value can exist at a time (although we can have numerous read-only references).
+        6. You can't mutate a value through the owner when any ref (mutable or immutable) to the value exists.
 */
