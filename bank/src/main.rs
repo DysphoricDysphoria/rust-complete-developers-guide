ef6e6e4540154b9346a77185b0a625d1dc85926a
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-10 11:32:03 +0530

    Move function around.

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -43,6 +43,23 @@ fn print_account_list_without_ref(accounts: Vec<Account>) {
     println!("{:#?}", accounts);
 }
 
+fn ref_demo_0() {
+    let account = Account::new(1, String::from("John Wick"));
+
+    // Multiple references can co-exist for a value
+    let account_ref_1 = &account;
+    let account_ref_2 = &account;
+
+    // account_ref_1.balance = 100; // !Error: Can't use reference to update value
+
+    // let other_account = account; // !Error: Can't move a value while reference(s) to the value exist (Caveat: Can move if the reference isn't used later on)
+
+    print_account(account_ref_1);
+    print_account(account_ref_2);
+
+    // println!("{:#?}", account);
+}
+
 fn ref_demo_1() {
     let bank = Bank::new();
     let account = Account::new(1, String::from("John Wick 2"));
@@ -64,23 +81,6 @@ fn ref_demo_1() {
     // print_account_list_with_ref(bank_accounts_ref); // Can't use the reference once a value (bank.accounts) has been moved
 
     // println!("{:#?}", bank); // !Error: borrow of partially move value: `bank`
-}
-
-fn ref_demo_0() {
-    let account = Account::new(1, String::from("John Wick"));
-
-    // Multiple references can co-exist for a value
-    let account_ref_1 = &account;
-    let account_ref_2 = &account;
-
-    // account_ref_1.balance = 100; // !Error: Can't use reference to update value
-
-    // let other_account = account; // !Error: Can't move a value while reference(s) to the value exist (Caveat: Can move if the reference isn't used later on)
-
-    print_account(account_ref_1);
-    print_account(account_ref_2);
-
-    // println!("{:#?}", account);
 }
 
 fn main() {
