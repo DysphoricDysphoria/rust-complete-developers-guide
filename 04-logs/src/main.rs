c86e9d9d510b18604e3c0e286a3974fee71096d9
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-29 19:06:22 +0530

    Add notes for strings, string refs and string slices.

--- 04-logs/src/main.rs
+++ 04-logs/src/main.rs
@@ -2,6 +2,59 @@ use std::fs;
 use std::io::Error;
 
 /*
+    ### 74. + 75. (AGAIN) ###
+        - String
+            - Struct (pointer to value | length | capacity) in Stack
+            - Pointer points to value in 'Heap'
+            - Uses memory in Stack & Heap
+        - &String
+            - Reference to 'String' | Stored in Stack
+            - Uses memory in Stack
+        - &str (String slice)
+            - Struct (pointer to value | length) in Stack
+            - Pointer points to value in 'Heap' or 'Data'
+              (Heap-allocated or Data-allocated text)
+            - Uses memory in Stack
+
+        - String to String slice
+            let color = String::from("red");
+            let c = color.as_str();
+                - In this case 'c' points to "red" in 'Heap'!
+
+        - &String and &str - both provide a read-only reference to text
+          data - Why two different types?
+            - &str lets you refer to text in the data segment without a
+              'Heap' allocation => slightly more performant
+                - If we do it solely via String, it will be a lot more
+                  work
+            - &str lets you 'slice' (take a portion) of text that is
+              already on the heap
+
+        - Using String
+            let color = String::from("red");
+            - Use anytime we want ownership of text
+            - Use anytime we want text that can grow or shrink
+
+        - Using &String
+            let color = String::from("red");
+            let color_ref = &color;
+            - Rarely used (usually never)
+            - Rust will automatically turn &String into &str for you
+
+        - Using &str
+            let color = String::from("red");
+            let c = color.as_str();
+            - Use anytime you don't want to take ownership of text
+            - Use anytime you want to refer to a portion of a string
+              owned by something else.
+
+        - String slices can either point at text stored in the data
+          segment or text stored in the heap that belongs to a String
+*/
+
+fn string_test(a: String, b: &String, c: &str) {}
+
+/*
     ### Result enum ###
         - Result enum is used when we need to know if something worked or failed
             - Ok() variant is used when something went well
@@ -45,6 +98,59 @@ fn result_demo_1() {
     }
 }
 
+fn string_demo_1() {
+    string_test(String::from("red"), &String::from("red"), "red");
+
+    string_test(
+        "blue".to_string(),
+        &String::from("blue"),
+        String::from("blue").as_str(),
+    );
+
+    // Here, in case of 3rd parameter rust is automatically
+    // converting &String to &str.
+    // If we pass reference to a String and a String slice
+    // is expected, rust will automatically do that for us.
+    string_test(
+        "blue".to_string(),
+        &String::from("blue"),
+        &String::from("blue"),
+    );
+}
+
+/*
+    ### 73. The Stack and Heap (AGAIN) ###
+        - Stack
+            - Fast, but limited size (2 - 8 MB)
+        - Heap
+            - Slow, but can grow to store a lot of data
+        - Data
+            - Stores literal values that we write into our code
+                - Ex: let num = 45; let color = "red"; => 45
+                  and "red" are stored into 'Data'
+        - Example
+            - let numbers = vec![1, 2, 3, 4, 5];
+                - The raw values 1, 2, 3, 4, 5 will initially be
+                  stored into 'Data' then later on copied into
+                  the 'Heap'?
+            - Super common pattern
+                - Stack stores metadata about a data structure
+                  (in this case 'numbers')
+                    - pointer to value(s) | length | capacity
+                - Heap stores the actual data
+                    - Actual values of the vector are stored here
+                - Avoids running out of memory in the stack if the
+                  data structure grows to hold a lot of data
+        - Corner case
+            - If a data structure owns another data structure, the
+              child's metadata will be placed on the heap
+            - Ex: let vec_of_num = vec![ vec![1, 2, 3, 4, 5] ];
+                - Metadata for nested vector will be stored inside
+                  the 'Heap'
+                - Metadata for parent vector will be stored inside
+                  the 'Stack'
+*/
+
 fn main() {
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => {
@@ -58,6 +164,8 @@ fn main() {
     println!();
 
     result_demo_1();
+
+    string_demo_1();
 }
 
 // Result is a generic 'enum'
