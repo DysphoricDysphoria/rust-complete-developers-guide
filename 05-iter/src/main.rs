163fb067fdb2bda9245be04bb0f3be6094b7c338
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-31 16:11:11 +0530

    Add new probject for iterators; add basic notes.

---
+++ 05-iter/src/main.rs
@@ -0,0 +1,13 @@
+/*
+    ### Iterator ###
+        - Used to iterate over any kind of data structure
+        - We've already been using them - they are used
+          behind the scenes when you write a for loop
+        - Follow all the same rules of ownership,
+          borrowing, lifetimes
+        - Use the Option enum
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
