088723012425d83b5e6b6d5be5c23b62ecdfe0ca
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-31 15:06:16 +0530

    Refactor methods of reading file to separate funcs.

--- 04-logs/src/main.rs
+++ 04-logs/src/main.rs
@@ -170,6 +170,38 @@ fn string_demo_1() {
     );
 }
 
+fn read_file_via_match() {
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => {
+            /*
+                - We could have also used '&text_that_was_read';
+                  'text_that_was_read.as_str()'
+                - Also, why does &String::from(text_that_was_read)
+                  throws an error?
+                    - extract_errors(text: &str) -> Vec<&str>
+                    - results.push(line)
+            */
+            let error_logs = extract_errors(text_that_was_read.as_str());
+
+            match fs::write("errors-1.txt", error_logs.join("\n")) {
+                Ok(..) => println!("Wrote errors-1.txt"),
+                Err(reason_write_failed) => {
+                    println!("Writing of errors-1.txt failed: {}", reason_write_failed)
+                }
+            }
+        }
+        Err(reason_text_was_not_read) => {
+            println!("Failed to read file: {}", reason_text_was_not_read)
+        }
+    }
+}
+
+fn read_file_via_expect() {
+    let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors-2.txt", error_logs.join("\n")).expect("failed to write errors-2.txt");
+}
+
 fn extract_errors(text: &str) -> Vec<String> {
     /*
         ### 77. Understanding the Issue ###
@@ -205,37 +237,9 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
-    let error_logs = extract_errors(text.as_str());
-    fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
+    read_file_via_match();
 
-    let use_match_statement = false;
-
-    if use_match_statement {
-        match fs::read_to_string("logs.txt") {
-            Ok(text_that_was_read) => {
-                /*
-                    - We could have also used '&text_that_was_read';
-                      'text_that_was_read.as_str()'
-                    - Also, why does &String::from(text_that_was_read)
-                      throws an error?
-                        - extract_errors(text: &str) -> Vec<&str>
-                        - results.push(line)
-                */
-                let error_logs = extract_errors(text_that_was_read.as_str());
-
-                match fs::write("errors.txt", error_logs.join("\n")) {
-                    Ok(..) => println!("Wrote errors.txt"),
-                    Err(reason_write_failed) => {
-                        println!("Writing of errors.txt failed: {}", reason_write_failed)
-                    }
-                }
-            }
-            Err(reason_text_was_not_read) => {
-                println!("Failed to read file: {}", reason_text_was_not_read)
-            }
-        }
-    }
+    read_file_via_expect();
 
     println!();
 
