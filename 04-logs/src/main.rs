4e10cc747484d12f13dff4c89fc5924b4cc12740
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-10-31 14:56:53 +0530

    Use alternate method of expect to handle errors as well.

--- 04-logs/src/main.rs
+++ 04-logs/src/main.rs
@@ -205,27 +205,35 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            /*
-                - We could have also used '&text_that_was_read';
-                  'text_that_was_read.as_str()'
-                - Also, why does &String::from(text_that_was_read)
-                  throws an error?
-                    - extract_errors(text: &str) -> Vec<&str>
-                    - results.push(line)
-            */
-            let error_logs = extract_errors(text_that_was_read.as_str());
+    let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
 
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(..) => println!("Wrote errors.txt"),
-                Err(reason_write_failed) => {
-                    println!("Writing of errors.txt failed: {}", reason_write_failed)
+    let use_match_statement = false;
+
+    if use_match_statement {
+        match fs::read_to_string("logs.txt") {
+            Ok(text_that_was_read) => {
+                /*
+                    - We could have also used '&text_that_was_read';
+                      'text_that_was_read.as_str()'
+                    - Also, why does &String::from(text_that_was_read)
+                      throws an error?
+                        - extract_errors(text: &str) -> Vec<&str>
+                        - results.push(line)
+                */
+                let error_logs = extract_errors(text_that_was_read.as_str());
+
+                match fs::write("errors.txt", error_logs.join("\n")) {
+                    Ok(..) => println!("Wrote errors.txt"),
+                    Err(reason_write_failed) => {
+                        println!("Writing of errors.txt failed: {}", reason_write_failed)
+                    }
                 }
             }
-        }
-        Err(reason_text_was_not_read) => {
-            println!("Failed to read file: {}", reason_text_was_not_read)
+            Err(reason_text_was_not_read) => {
+                println!("Failed to read file: {}", reason_text_was_not_read)
+            }
         }
     }
 
