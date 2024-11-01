243cac124a580b758e85e89bbae2e86a8ee0c6ec
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-01 13:04:37 +0530

    Add basic notes for iter.

--- 05-iter/src/main.rs
+++ 05-iter/src/main.rs
@@ -9,5 +9,33 @@
 */
 
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+    ];
+
+    /*
+        - colors.iter() creates a new data structure
+          Iter<String>
+            - Iter is separate from the Vector etc.
+              we are iterating over
+            - Iter is a struct which has a couple
+              of fields inside it
+                - Pointer to data (Vec<String>, in this
+                  case)
+                - Pointer to current position ("red")
+                - Pointer to end (pointing outside the
+                  bounds of vector)
+            - next() => Some() or None
+    */
+    let mut colors_iter = colors.iter();
+    // We need to use 'mut' above because we are changing
+    // something (pointer to current position) within the
+    // iterator every time we use 'next()'
+
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
 }
