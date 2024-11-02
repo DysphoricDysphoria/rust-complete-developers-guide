4bbd3bf3ed2b8e911e238f5ee3070c43a6dec8cc
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-02 13:29:19 +0530

    Add starter notes for lifetimes.

---
+++ 06-lifetimes/src/main.rs
@@ -0,0 +1,12 @@
+/*
+    ### Lifetime annotations ###
+        - Used with functions, structs, enums, and more
+        - Help the compiler make sure refs won't outlive
+        the value they refer to
+        - This will seem like something the compiler
+        should do on its own
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
