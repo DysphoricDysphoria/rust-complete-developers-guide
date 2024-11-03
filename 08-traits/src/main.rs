e967c0a46795a2912673bfddf01c63a702f26736
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-03 18:23:31 +0530

    Add generic add_string to add items to Basket or Stack.

--- 08-traits/src/main.rs
+++ 08-traits/src/main.rs
@@ -3,16 +3,39 @@ mod container;
 mod stack;
 
 use basket::Basket;
+use container::Container;
 use stack::Stack;
 
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
+}
+
 fn main() {
-    let b1 = Basket::<String>::new(String::from("Hi there!"));
+    let mut b1 = Basket::<String>::new(String::from("Hi there!"));
 
     let b2 = Basket::new(10);
 
     let b3 = Basket::new(true);
 
-    let s1 = Stack::new(vec![String::from("Hi!")]);
+    let mut s1 = Stack::new(vec![String::from("Hi!")]);
 
     let s2 = Stack::new(vec![1, 2, 3]);
+
+    println!("b1 - before: {:#?}", b1);
+
+    println!();
+
+    add_string(&mut b1, String::from("Hello World!"));
+
+    println!("b1 - after: {:#?}", b1);
+
+    println!();
+
+    println!("s1 - before: {:#?}", s1);
+
+    println!();
+
+    add_string(&mut s1, String::from("Hello New World!"));
+
+    println!("s1 - after: {:#?}", s1);
 }
