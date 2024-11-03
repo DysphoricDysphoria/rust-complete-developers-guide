3eef84d69dd3a34757702bd67eb08739749d1360
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-03 17:34:09 +0530

    Add stack module with Stack struct.

--- 08-traits/src/main.rs
+++ 08-traits/src/main.rs
@@ -1,11 +1,17 @@
 mod basket;
+mod stack;
 
 use basket::Basket;
+use stack::Stack;
 
 fn main() {
-    let b1 = Basket::new(String::from("Hi there!"));
+    let b1 = Basket::<String>::new(String::from("Hi there!"));
 
     let b2 = Basket::new(10);
 
     let b3 = Basket::new(true);
+
+    let s1 = Stack::new(vec![String::from("Hi!")]);
+
+    let s2 = Stack::new(vec![1, 2, 3]);
 }
