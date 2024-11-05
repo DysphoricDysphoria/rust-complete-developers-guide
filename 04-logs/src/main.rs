9cffaba07b0064697ad5baa3f90c0300731977d3
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-05 23:16:38 +0530

    Whitespace changes.

--- 04-logs/src/main.rs
+++ 04-logs/src/main.rs
@@ -7,53 +7,53 @@ use std::io::Error;
             - Fast, but limited size (2 - 8 MB)
         - Heap
             - Slow, but can grow to store a lot of data (Gigabytes
-              worth of data)
+            worth of data)
         - Data/Data segment/Read-only-data segment/Static segment
             - Stores literal values that we write directly into our
-              source code
+            source code
                 - Ex: let num = 45; let color = "red"; => 45 and
-                  "red" are stored into 'Data'
+                "red" are stored into 'Data'
         - Ex:
             - let numbers = vec![1, 2, 3, 4, 5];
                 - The initial values 1, 2, 3, 4, 5 will be stored
-                  into 'Data' and when we make the vector, these
-                  will be copied into the 'Heap'
-                  - During parsing, rust might create 1 - 5
+                into 'Data' and when we make the vector, these
+                will be copied into the 'Heap'
+                    - During parsing, rust might create 1 - 5
                     in 'Data' and then later copy it into the 'Heap'?
             - Super common pattern
                 - Stack stores metadata about a data structure
-                  (in this case 'numbers')
+                (in this case 'numbers')
                     - pointer to value(s) | length | capacity
                 - Heap stores the actual data
                     - Actual values of the vector are stored here
                 - Avoids running out of memory in the stack if the
-                  data structure grows to hold a lot of data
+                data structure grows to hold a lot of data
         - Corner case
             - If a data structure owns another data structure, the
-              child's metadata will be placed on the heap
+            child's metadata will be placed on the heap
             - Ex: let vec_of_num = vec![ vec![1, 2, 3, 4, 5] ];
                 - Metadata for nested vector will be stored inside
-                  the 'Heap'
+                the 'Heap'
                 - Metadata for parent vector will be stored inside
-                  the 'Stack'
+                the 'Stack'
 */
 
 /*
     ### 74. Strings, String Refs, and String Slices ###
         - String
             - Struct (pointer to text | length of string in
-              heap | capacity of string in heap) in Stack
+            heap | capacity of string in heap) in Stack
             - Pointer points to value in 'Heap'
             - Uses memory in 'Stack' & 'Heap'
         - &String (read-only)
             - Reference to 'String' (All the work
-              of String + REFERENCE) | Stored in Stack
+            of String + REFERENCE) | Stored in Stack
             - Uses memory in 'Stack'
         - &str (String slice - read-only)
             - Struct (pointer to text | length of string) in
-              Stack
+            Stack
             - Pointer points to value in 'Heap' or 'Data'
-              (Heap-allocated or Data-allocated text)
+            (Heap-allocated or Data-allocated text)
             - Uses memory in 'Stack'
             - String to String slice
                 let color = String::from("red");
@@ -64,20 +64,20 @@ use std::io::Error;
 /*
     ### 75. When to Use Which String ###
         - &String and &str - both provide a read-only reference to text
-          data - Why two different types?
+        data - Why two different types?
             - &str lets you refer to text in the data segment without a
-              'Heap' allocation => slightly more performant
+            'Heap' allocation => slightly more performant
                 - If we do it solely via 'String', it will be a lot more
-                  work
+                work
             - &str lets you 'slice' (take a portion) of text that is
-              already on the heap
+            already on the heap
                 let color = String::from("blue"); => String
                 let portion = &color[1..4]; => &str
                 - When we do &color[1..4], behind the scenes '&str' is
-                  created in Stack and &color[1..4] refers to 'lue'
-                  portion of the same heap-allocated 'blue'
+                created in Stack and &color[1..4] refers to 'lue'
+                portion of the same heap-allocated 'blue'
                 - Again, it will be a lot more work is we do this via
-                  'String'
+                'String'
 
         - Usage
             - String
@@ -97,7 +97,7 @@ use std::io::Error;
                 owned by something else.
 
         - String slices can either point at text stored in the 'Data
-          segment' or text stored in the 'Heap' that belongs to a String
+        segment' or text stored in the 'Heap' that belongs to a String
 
 */
 
