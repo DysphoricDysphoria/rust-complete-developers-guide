b65ca40d969de75c0680b4632d26bcf682abb454
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-06 18:35:33 +0530

    Proofreading.

--- 04-logs/src/main.rs
+++ 04-logs/src/main.rs
@@ -95,9 +95,9 @@ use std::io::Error;
                 - Use anytime you don't want to take ownership of text
                 - Use anytime you want to refer to a 'portion' of a string
                 owned by something else.
-        
+
         - String slice (&str) can either point at text stored in the
-        'Data segment' or text stored in the 'Heap' that belongs to 
+        'Data segment' or text stored in the 'Heap' that belongs to
         a String
 */
 
