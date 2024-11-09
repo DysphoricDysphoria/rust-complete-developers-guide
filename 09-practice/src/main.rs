735af35d6e94174754cfc8082e8883ebaf927c03
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-10 00:20:28 +0530

    Review 05-iter - 1

--- 09-practice/src/main.rs
+++ 09-practice/src/main.rs
@@ -5,7 +5,110 @@
         behind the scenes when you write a for loop
         - Follow all the same rules of ownership,
         borrowing, lifetimes
-        - Use the Option enum
+        - Use the 'Option' enum
 */
 
-fn main() {}
+/*
+    ### We usually don't call 'next' on an iterator
+        manually. Instead... ###
+        - Option 1: Use a for loop. Automatically creates
+        an iterator and calls 'next' on it
+        - Option 2: Use iterator adaptors and consumers
+        like 'for_each', 'collect', 'map', etc.
+*/
+
+// TODO: Clippy with rust-analyzer?
+
+/*
+    ### Vector slice - &[<Type>] ###
+        - Vec<String> => &[String]
+        - It is a struct
+            - Pointer to data
+            - Length
+        - Similar to 'String slice' and avoids the same
+        set of problems
+        - Why use 'print_elements' with 'Vector slice'?
+            - Single function that can work with either
+            a full vector or just a portion of a vector
+*/
+
+fn print_elements(elements: &[String]) {
+    let use_for_loop = false;
+
+    if use_for_loop {
+        /*
+            ### for loop will ... ###
+                - Automatically create an iterator for the
+                vector
+                - Call 'next' on the vector and unwrap the
+                Option that comes back
+                - Break once 'next' returns a None
+        */
+        for element in elements {
+            println!("{}", element);
+        }
+    } else {
+        let use_iter_consumer = true;
+
+        if use_iter_consumer {
+            /*
+                ### Iterator consumers ###
+                    - Iterators are 'lazy', Nothing happens until...
+                        - We call 'next'
+                        - We use a function that calls 'next' automatically
+                            - These functions are called consumers (they
+                            are (kinda?) consuming values out of the
+                            iterator). Ex: for_each()
+                            - for_each() is an iterator 'consumer'
+                            - It will repeatedly call 'next()' on the
+                            iterator until it gets 'None'
+            */
+            elements.iter().for_each(|el| println!("{}", el));
+        }
+    }
+}
+
+fn main() {
+    let colors = vec![
+        String::from("blue"),
+        String::from("green"),
+        String::from("red"),
+    ];
+
+    /*
+        - color.iter() creates a new data structure
+        Iter<String>
+            - Iter is separate from the Vector etc.
+            we are iterating over
+            - Iter is a struct which has a couple
+            of fields inside it
+                - Pointer to data (Vec<String>, in this
+                case)
+                - Pointer to current position ("red")
+                - Pointer to end (pointing outside the
+                bounds of vector)
+            - next() => Some() or None
+    */
+    let mut colors_iter = colors.iter();
+    // We need to use 'mut' above because we are changing
+    // something (pointer to current position) within the
+    //  iterator every time we use 'next()'
+
+    println!("colors_iter.next() {:#?}", colors_iter.next());
+    println!("colors_iter.next() {:#?}", colors_iter.next());
+    println!("colors_iter.next() {:#?}", colors_iter.next());
+    println!("colors_iter.next() {:#?}", colors_iter.next());
+
+    println!();
+
+    // With 'print_elements(elements: &Vec<String>)'
+    // we are only allowed to pass in the full Vector.
+    // If we modify it to 'print_elements(elements:
+    // &[String])', we can pass in a portion of the
+    // Vector as well
+    print_elements(&colors);
+
+    println!();
+
+    print_elements(&colors[1..3]);
+}
