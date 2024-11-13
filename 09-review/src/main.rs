beb3a892f629f336dfc11906b0e56cf567dff89b
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-13 23:12:30 +0530

    Finish review for 08-traits

--- 09-review/src/main.rs
+++ 09-review/src/main.rs
@@ -1,93 +1,41 @@
-use num_traits::ToPrimitive;
+mod basket;
+mod container;
+mod stack;
 
-/*
-    ### 'solve' function ###
-        - First version - We can pass in both f32 & f64
-        via 'Float' trait
-        - Second version - We can pass in any type of
-        number via 'ToPrimitive' trait
+use basket::Basket;
+use container::Container;
+use stack::Stack;
 
-    ### Trait bounds ###
-        - 'Float' is a 'trait'
-        - 'Float' is being used as a 'trait bound'
-
-        ### What is a trait? ###
-        - A trait is a set of methods
-        - It can contain abstract methods which don't have
-        implementation
-        - It can contain default methods which have
-        implementation
-
-        - A struct/enum/primitive can implement a trait
-        - The implementor has to provide an implementation
-        for all of the abstract methods
-        - The implementor can optionally override the
-        default methods
-
-        - Behind the scenes, f32 and f64 both implement the
-        'Float' trait
-
-    ### Super Solve Flexibility ###
-        - Use 'ToPrimitive' trait instead of 'Float' trait
-        in 'solve' func
-*/
-
-trait Vehicle {
-    // Abstract method
-    fn start(&self);
-
-    // Default method
-    fn stop(&self) {
-        println!("Stopped");
-    }
-}
-
-struct Car {}
-
-impl Vehicle for Car {
-    fn start(&self) {
-        println!("Started");
-    }
-}
-
-fn start_and_stop<T: Vehicle>(vehicle: T) {
-    vehicle.start();
-    vehicle.stop();
-}
-
-// If we use 'T: ToPrimitive' and a: T, b: T and pass in an
-// integer and a float as arguments we get an error. This is
-// so because a: T, b: T implies that 'a' and 'b' are both of
-// similar type
-fn solve<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
-    // Works with 'use num_traits::ToPrimitive;'
-    let a_f64 = a.to_f64().unwrap(); // !Error: 'a as f64'
-
-    // Works with 'use num_traits::ToPrimitive;'
-    let b_f64 = b.to_f64().unwrap();
-
-    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
 }
 
 fn main() {
-    // Can't take different type of numbers and do
-    // arithmetic on them
-    let a: f32 = 3.0;
-    let b: f64 = 4.0;
+    let mut b1 = Basket::<String>::new(String::from("Hi there!"));
 
-    println!("{}", solve::<f32, f64>(a, b));
+    let b2 = Basket::new(10);
 
-    // Here, we are relying upon inference
-    println!("{}", solve(6.0, 8.0));
+    let b3 = Basket::new(true);
 
-    println!();
+    let mut s1 = Stack::new(vec![String::from("Hi!")]);
 
-    let car = Car {};
-    start_and_stop(car);
+    let s2 = Stack::new(vec![1, 2, 3]);
+
+    println!("b1 - before: {:#?}", b1);
 
     println!();
 
-    // Super solve flexibility (with 'ToPrimitive')
-    // This won't work with 'Float' trait
-    println!("{}", solve(30, 40.0));
+    add_string(&mut b1, String::from("Hello World!"));
+
+    println!("b1 - after: {:#?}", b1);
+
+    println!();
+
+    println!("s1 - before: {:#?}", s1);
+
+    println!();
+
+    add_string(&mut s1, String::from("Hello New World!"));
+
+    println!("s1 - after: {:#?}", s1);
 }
