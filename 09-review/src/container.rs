baf52e0e2335622275e4cfb0dbe7ac7761da7d2a
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-16 01:14:44 +0530

    Finish review for 01-deck.

--- 09-review/src/container.rs
+++
@@ -1,5 +0,0 @@
-pub trait Container<T> {
-    fn get(&mut self) -> Option<T>;
-    fn put(&mut self, item: T);
-    fn is_empty(&self) -> bool;
-}
--- 09-review/src/main.rs
+++ 09-review/src/main.rs
@@ -1,41 +1,82 @@
-mod basket;
-mod container;
-mod stack;
+use rand::{seq::SliceRandom, thread_rng};
 
-use basket::Basket;
-use container::Container;
-use stack::Stack;
+// This is a derive attribute. This enhances the
+// functionality of our struct.
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
 
-fn add_string<T: Container<String>>(c: &mut T, s: String) {
-    c.put(s);
+/*
+    ### Associated functions and methods ###
+        - Associated functions are functions that are
+        associated with the struct itself, not instances
+        of the struct.
+        - Methods are functions that are associated with
+        instances of the struct.
+*/
+
+// Inherent implementation block
+impl Deck {
+    // We can also use '-> Deck' instead of '-> Self' in
+    // the function signature.
+    // 'new' is an associated function
+    fn new() -> Self {
+        // List of suits
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+        // List of values
+        let values = ["Ace", "Two", "Three"];
+
+        // Variables are 'bindings' in Rust
+        let mut cards = Vec::new();
+        // Without 'mut' we can't reassign or change the
+        // value of bindings.
+        // Vec::new() is the same as vec![]
+
+        // Double nested for loop to create a deck of cards
+        // for each suit in suits
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        // Implicit return - Rust automatically returns
+        // the last expression in a block (without a
+        // semicolon!)
+        Deck { cards }
+    }
+
+    // Crate == Package
+    // 'shuffle' is a method
+    fn shuffle(&mut self) {
+        // thread_rng() is a function that returns a random
+        // number generator
+        let mut rng = thread_rng();
+
+        // shuffle() is a method that shuffles the deck
+        self.cards.shuffle(&mut rng);
+    }
+
+    // 'deal' is a method
+    fn deal(&mut self, num_cards_to_remove: usize) -> Vec<String> {
+        let removal_idx = self.cards.len() - num_cards_to_remove;
+        self.cards.split_off(removal_idx)
+    }
 }
 
 fn main() {
-    let mut b1 = Basket::<String>::new(String::from("Hi there!"));
+    let mut deck = Deck::new();
 
-    let b2 = Basket::new(10);
+    // Shuffle the deck
+    deck.shuffle();
 
-    let b3 = Basket::new(true);
+    // TODO: Need to add error handling; try using 100
+    // instead of 3 => deal(100)
+    let cards_via_deal = deck.deal(3);
 
-    let mut s1 = Stack::new(vec![String::from("Hi!")]);
-
-    let s2 = Stack::new(vec![1, 2, 3]);
-
-    println!("b1 - before: {:#?}", b1);
-
-    println!();
-
-    add_string(&mut b1, String::from("Hello World!"));
-
-    println!("b1 - after: {:#?}", b1);
-
-    println!();
-
-    println!("s1 - before: {:#?}", s1);
-
-    println!();
-
-    add_string(&mut s1, String::from("Hello New World!"));
-
-    println!("s1 - after: {:#?}", s1);
+    println!("Here's your hand: {:#?}", cards_via_deal);
+    println!("Here's your deck: {:#?}", deck.cards);
 }
