5658a204324c831e1e2bd33a354c6fe8cb76f4ae
DysphoricDysphoria <anmolkhurana91@gmail.com>
2024-11-19 23:47:08 +0530

    Ongoing review for 02-bank.

--- 09-review/src/main.rs
+++ 09-review/src/main.rs
@@ -54,24 +54,29 @@ impl Bank {
 
     fn total_balance(&self) -> i32 {
         // ### Method 1 ###
-        let mut total = 0;
-        for acc in &self.accounts {
-            total += acc.balance;
-        }
-        total
+        // let mut total = 0;
+        // for acc in &self.accounts {
+        //     total += acc.balance;
+        // }
+        // total
 
-        // TODO: ### Method 2 ###
+        // ### Method 2 ###
+        self.accounts.iter().map(|account| account.balance).sum()
     }
 
     fn summary(&self) -> Vec<String> {
         // ### Method 1 ###
-        let mut summary = Vec::new();
-        for acc in &self.accounts {
-            summary.push(acc.summary());
-        }
-        summary
+        // let mut summary = Vec::new();
+        // for acc in &self.accounts {
+        //     summary.push(acc.summary());
+        // }
+        // summary
 
-        // TODO: ### Method 2 ###
+        // ### Method 2 ###
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
     }
 }
 
@@ -82,12 +87,12 @@ fn print_account(account: &Account) {
 
 // This func accepts a reference
 fn print_account_list_with_ref(accounts: &Vec<Account>) {
-    print!("{:#?}", accounts);
+    println!("{:#?}", accounts);
 }
 
 // This func accepts a value
 fn print_account_list_without_ref(accounts: Vec<Account>) {
-    print!("{:#?}", accounts);
+    println!("{:#?}", accounts);
 }
 
 // This func accepts a mutable reference
@@ -121,8 +126,8 @@ fn ref_demo_0() {
     // account_ref_1.balance = 100;
 
     // !Error: Can't move a value while reference(s) to
-    // the value exist (Caveat: Can move if the reference)
-    // isn't used later on
+    // the value exist (Caveat: Can move if the reference
+    // isn't used later on)
     // let other_account = account;
 
     print_account(account_ref_1);
@@ -155,7 +160,7 @@ fn ref_demo_1() {
             ### Answer ###
             Rust allows you to borrow part of a struct
             (like a field) and later move the entire field
-            out of the struct, as long as you DON"T USE THE
+            out of the struct, as long as you DON'T USE THE
             REFERENCE AFTER THE MOVE.
     */
     print_account_list_without_ref(bank.accounts);
@@ -172,17 +177,17 @@ fn ref_demo_2() {
     let mut account = Account::new(1, String::from("Betty Davis"));
 
     // !Error: We can't have a read-only (immutable)
-    // reference if we have a mutable reference or
+    // reference if we have a mutable reference and
     // vice-versa; depends on what is initialized
     // and used first
-    let account_ref_read_only = &account;
+    // let account_ref_read_only = &account;
 
     // Mutable reference
     let account_ref_mutable_1 = &mut account;
 
     // !Error: Can't modify a binding via its owner if we
     // have a reference (mutable or immutable)
-    account.balance = 100;
+    // account.balance = 100;
 
     // !Error: We can't have another mutable reference if
     // we already have one
@@ -195,9 +200,17 @@ fn ref_demo_2() {
     // println!("{:#?}", account_ref_read_only.holder);
 }
 
-fn ref_demo_3() {}
+fn ref_demo_3() {
+    let num = 5;
 
-fn lifetimes_demo_0() {}
+    // Ownership isn't transferred from num to other_num
+    let other_num = num;
+    println!("{} {}", num, other_num);
+}
+
+fn lifetimes_demo_0() {
+    let _account = make_and_print_account();
+}
 
 fn demos() {
     ref_demo_0();
@@ -242,3 +255,15 @@ fn main() {
 
     println!("{:#?}", bank);
 }
+
+/*
+    ### ### ### ### #
+    ### Ownership ###
+    ### ### ### ### #
+    - The goal of ownership is to limit the ways you can
+    reference and change data.
+        > Lesson 1: Multiple things can refer to a value
+        at the same time, but they are all read-only
+        > Lesson 2: A value can only be updated when there
+        are no read-only/write references to it
+*/
